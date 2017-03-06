f2241888730c37c80e66cd8357d65b91610ff04b
retorta <retortakolba@yandex.ru>
2017-03-07 01:38:00 +0300

    more elephants

--- zzz.js
+++ zzz.js
@@ -19,7 +19,55 @@ var railroadCar = [ "/images/emoji/D83DDC4D.png",
                     ]
 var elephant = [ "https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg",
                  "http://www.livescience.com/images/i/000/036/988/original/elephants.jpg",
-                 "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/african-elephant-herd.jpg"
+                 "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/african-elephant-herd.jpg",
+                 "http://www.planwallpaper.com/static/images/2048-5.jpg",
+                 "https://shorganics.files.wordpress.com/2015/03/o-elephant-facebook.jpg",
+                 "http://s2.thingpic.com/images/Lr/ZL6NoebHz54iTNPWnZGU86ik.jpeg",
+                 "http://www.wildaid.org/sites/default/files/photos/iStock_000007961808Large.jpg",
+                 "http://www.wildaid.org/sites/default/files/photos/elephants_Peter%20Knights.JPG",
+                 "http://www.planwallpaper.com/static/images/TWhC7.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/736x/94/a1/92/94a192e8757f339826d28c2b701c3822.jpg",
+                 "https://www.zsl.org/sites/default/files/media/2014-10/SG9A8359.jpg",
+                 "http://images.nationalgeographic.com/wpf/media-live/photos/000/002/custom/baby-asian-elephant_227_610x343.jpg",
+                 "http://pmdvod.nationalgeographic.com/NG_Video/641/303/Baby_Elephants_FINAL_640x360_515420227659.jpg",
+                 "https://i.ytimg.com/vi/SNggmeilXDQ/maxresdefault.jpg",
+                 "https://pbs.twimg.com/media/Cprkjh6WEAAS7RP.jpg",
+                 "https://images.jg-cdn.com/image/89819fa3-a44e-489d-990c-ea07e14391a2.jpg?template=fundraisingpagegalleryxl",
+                 "http://www.hd-freewallpapers.com/elephant-wallpaper/desktop-images-of-elephant-cartoon-download.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/originals/40/27/ba/4027bad8c5eb49983bf33f42332f7eed.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/originals/d3/c2/2c/d3c22c5b389eb427de43a2bc21f250ba.jpg",
+                 "http://www.wildaid.org/sites/default/files/photos/Elephant%20Pair_PKnights.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/736x/bf/30/2e/bf302e2745ecdb74cf3c0a9aea1af73f.jpg",
+                 "http://www.earthtimes.org/newsimage/elephants-diet_15212.jpg",
+                 "http://cdn.wallpapersafari.com/31/27/VoeQE2.jpg",
+                 "http://www.gannett-cdn.com/-mm-/34211b60883e38fe4f727f9cdc9311a523bbf2aa/c=0-99-3186-1899&r=1280x720&r=540/local/-/media/2016/09/26/USATODAY/USATODAY/636104844984837687-AP-South-Africa-Wildlife-Drought.jpg",
+                 "https://cbsdallas.files.wordpress.com/2011/07/dallas-zoo-mama-stumpy-5-10.jpg",
+                 "http://saveafricaselephants.com/wp-content/uploads/2013/11/elephant-mother-baby-1200x600.jpg",
+                 "http://www.sabisabi.com/blog/wp-content/uploads/2013/01/Elephant-twins-3.jpg",
+                 "http://assets.inhabitat.com/wp-content/blogs.dir/1/files/2014/12/Baby-Elephant.jpg",
+                 "http://twistedsifter.files.wordpress.com/2010/12/baby-asian-elephant.jpg",
+                 "http://blog.africageographic.com/africa-geographic-blog/files/2013/04/Kaza-Elephants.gif",
+                 "https://s-media-cache-ak0.pinimg.com/736x/af/1a/d2/af1ad264d336901d71dad4375a2605c8.jpg",
+                 "http://farm4.static.flickr.com/3577/3366218871_ff24e05ea2_b.jpg",
+                 "http://www.post-gazette.com/image/2016/05/02/ca0,0,2210,1493/8wq00kk1-JPG.JPG",
+                 "https://photosbyravi.files.wordpress.com/2013/06/an-elephant-mothers-touch.jpg",
+                 "http://advocacy.britannica.com/blog/advocacy/wp-content/uploads/elephant-3.jpg",
+                 "https://cdn.theatlantic.com/assets/media/img/photo/2015/08/world-elephant-day/e18_AP051104010452/main_900.jpg?1439404108",
+                 "https://i.ytimg.com/vi/r_wZ0ZSUbL4/maxresdefault.jpg",
+                 "http://www.nationalgeographic.com/tracking-ivory/assets/img/article_og.jpg",
+                 "http://7art-screensavers.com/screenshots/elephants/calf-and-elephant.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/736x/59/94/c2/5994c2c50043b77cffc582d91f90a3ad.jpg",
+                 "http://az616578.vo.msecnd.net/files/2016/05/19/6359929346867053911395224638_elephants-11.jpg",
+                 "http://i.dailymail.co.uk/i/pix/2010/02/22/article-1252912-084B7D63000005DC-51_468x306.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/originals/ea/71/37/ea7137210f95d1e55bc3a307df40113d.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/736x/4a/10/d0/4a10d07ccc0cb9ecc56e4c28db45319b.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/originals/d2/6f/68/d26f687277c0fabc671e62158767faa0.jpg",
+                 "https://s-media-cache-ak0.pinimg.com/originals/94/8c/98/948c98675f3c157a71a174a17f0b8a2a.jpg",
+                 "http://awsassets.wwf.org.za/img/original/elephant.jpg",
+                 "http://www.artsmartiauxcombat.com/rubriques/cute-baby-elephant-pictures-i6.jpg",
+                 
+                 
+                 
                 
                 ]
 var locomotive = "/images/emoji/D83DDE82.png";
@@ -82,13 +130,42 @@ function zzz()
     // pictures
     if(true)
     {
-        pic = document.getElementsByTagName('img')
+        pic = document.getElementsByTagName('img');
         for(i = 0; i < pic.length; i++)
         {
             if (! pic[i].getAttribute("elephant"))
             {
                 pic[i].setAttribute("src", elephant[Math.floor(Math.random() * elephant.length)])
                 pic[i].setAttribute("elephant",true)
+                pic[i].removeAttribute("height")
+                pic[i].removeAttribute("width")
+            }
+        }
+        pic = document.getElementsByTagName("*");
+        for(i = 0; i < pic.length; i++)
+        {
+            if (pic[i].getAttribute("style"))
+            {
+                style = pic[i].getAttribute("style")
+                if(style.indexOf("background-image:") != -1 && !pic[i].getAttribute("elephant"))
+                {
+                    outstyle = ""
+                    style = style.split(';')
+                    for(j = 0; j < style.length; j++)
+                    {
+                        if(style[j].indexOf("background-image:") != -1)
+                        {
+                            outstyle += "background-image: url("+elephant[Math.floor(Math.random() * elephant.length)]+");"
+                        }
+                        else
+                        {
+                            outstyle += style[j]+";"
+                        }
+                    }                                   
+                    // console.log(style)
+                    pic[i].setAttribute("style", outstyle);
+                    pic[i].setAttribute("elephant", true);
+                }
             }
         }
         
